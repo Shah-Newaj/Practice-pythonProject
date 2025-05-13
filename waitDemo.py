8569985fa7b2592511f886b554ee86cf63bf7e51
Hasanul-Banna <hbshovon776@gmail.com>
2025-05-13 12:15:42 +0600

    Implicit and Explicit wait

--- waitDemo.py
+++ waitDemo.py
@@ -2,7 +2,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
