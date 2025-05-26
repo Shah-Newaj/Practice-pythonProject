dd4101df9ac3d131dd76362272e8604f6024637c
Shah Newaj <newajuiucse@gmail.com>
2025-05-26 11:25:58 +0600

    Use of fixture

--- pytestDemo/test_fixtureDemo.py
+++ pytestDemo/test_fixtureDemo.py
@@ -1,15 +1,19 @@
 #pytest -v -s .\pytestDemo\test_fixtureDemo.py
+# fixture are used as setup and tear down methods for test cases
+# conftest file used to generalize fixture and make it available to all test cases
+
 
 import pytest
 
-@pytest.fixture()
-def setup():
-    print("I will be executed first")
+@pytest.mark.usefixtures("setup")
+class TestExample:
 
-    yield
-    print("I will be executed last")
+    def test_fixtureDemo1(self):
+        print("I will execute steps in fixtureDemo1 method")
 
+    def test_fixtureDemo2(self):
+        print("I will execute steps in fixtureDemo2 method")
 
-def test_fixtureDemo(setup):
-    print("I will execute steps in fixtureDemo method")
+    def test_fixtureDemo3(self):
+        print("I will execute steps in fixtureDemo3 method")
 
